9da186966f6417d1df187935b66a97e4d1447fd8
HaloAssassin973 <cokturqi55@mail.ru>
2019-11-28 15:35:49 +0300

    Add network and icons

---
+++ KasperskyFoto/Network/NetworkDataFetcher.swift
@@ -0,0 +1,42 @@
+//
+//  NetworkDataFetcher.swift
+//  KasperskyFoto
+//
+//  Created by Игорь Силаев on 28/11/2019.
+//  Copyright © 2019 Игорь Силаев. All rights reserved.
+//
+
+import Foundation
+
+
+class NetworkDataFetcher {
+    
+    var networkService = NetworkService()
+    
+    func fetchImages(searchTerm: String, completion: @escaping (Foto?) -> ()) {
+        networkService.request(searchTerm: searchTerm) { (data, error) in
+            if let error = error {
+                print("Error received requesting data: \(error.localizedDescription)")
+                completion(nil)
+            }
+            
+            let decode = self.decodeJSON(type: Foto.self, from: data)
+            completion(decode)
+        }
+    }
+    
+    func decodeJSON<T: Decodable>(type: T.Type, from: Data?) -> T? {
+        let decoder = JSONDecoder()
+        guard let data = from else { return nil }
+        
+        do {
+            let objects = try decoder.decode(type.self, from: data)
+            return objects
+        } catch let jsonError {
+            print("Failed to decode JSON", jsonError)
+            return nil
+        }
+    }
+    
+    
+}
---
+++ KasperskyFoto/VC/PhotoViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PhotoViewCell.swift
+//  KasperskyFoto
+//
+//  Created by Игорь Силаев on 28/11/2019.
+//  Copyright © 2019 Игорь Силаев. All rights reserved.
+//
+
+import UIKit
+
+class PhotoViewCell: UICollectionViewCell {
+    @IBOutlet weak var photoImage: UIImageView!
+    
+}
