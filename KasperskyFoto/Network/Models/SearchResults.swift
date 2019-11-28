03efa88f8bebf7e81b8b5d5f2d5ac10345058d23
HaloAssassin973 <cokturqi55@mail.ru>
2019-11-28 22:00:17 +0300

    Update SearchResults.swift

--- KasperskyFoto/Network/Models/SearchResults.swift
+++ KasperskyFoto/Network/Models/SearchResults.swift
@@ -8,26 +8,6 @@
 
 import Foundation
 
-struct SearchResults: Decodable {
-    let total: Int
-    let results: [UnsplashPhoto]
-}
-
-struct UnsplashPhoto: Decodable {
-    let width: Int
-    let height: Int
-    let urls: [URLKing.RawValue:String]
-    
-    
-    enum URLKing: String {
-        case raw
-        case full
-        case regular
-        case small
-        case thumb
-    }
-}
-
 // MARK: - Foto
 struct Foto: Decodable {
     let totalHits: Int
