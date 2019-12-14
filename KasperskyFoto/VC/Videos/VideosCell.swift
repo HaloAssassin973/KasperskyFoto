ecd7914b4ace387d54d1458b9ba8cf4d959faac2
HaloAssassin973 <cokturqi55@mail.ru>
2019-12-14 18:01:28 +0300

    Remove Realm and some zoom

--- KasperskyFoto/VC/Videos/VideosCell.swift
+++ KasperskyFoto/VC/Videos/VideosCell.swift
@@ -11,34 +11,17 @@ import SDWebImage
 
 class VideosCell: UICollectionViewCell {
     
+    @IBOutlet weak var videoImageView: UIImageView!
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        setupPhotoImageView()
     }
-    
-     let photoImageView: UIImageView = {
-        let imageView = UIImageView()
-        imageView.translatesAutoresizingMaskIntoConstraints = false
-        imageView.backgroundColor = #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1)
-        imageView.contentMode = .scaleAspectFill
-        return imageView
-    }()
     
     var video: Hit? {
         didSet {
             let videoUrl = "https://i.vimeocdn.com/video/\(video?.pictureID ?? "773604276")_200x150.jpg"
             let url = URL(string: videoUrl)
-            photoImageView.sd_setImage(with: url, completed: nil)
+            videoImageView.sd_setImage(with: url, completed: nil)
         }
-    }
-    
-    
-    private func setupPhotoImageView() {
-        addSubview(photoImageView)
-        photoImageView.topAnchor.constraint(equalTo: self.topAnchor).isActive = true
-        photoImageView.bottomAnchor.constraint(equalTo: self.bottomAnchor).isActive = true
-        photoImageView.trailingAnchor.constraint(equalTo: self.trailingAnchor).isActive = true
-        photoImageView.leadingAnchor.constraint(equalTo: self.leadingAnchor).isActive = true
-        
     }
 }
